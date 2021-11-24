e7b8448e452b24341004b2c8c33e6c7313efef48
mllrdev-jerry-bayoneta <msd.jerry.bayoneta@gmail.com>
2021-11-24 14:10:29 +0800

    chore:added sufix -page for view page

---
+++ src/views/home-page.vue
@@ -0,0 +1,37 @@
+<template>
+  <Header />
+  <h1 @click="goToNewsFeed()">Hello</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/shared/component/header.vue";
+import RouteName from "@/enum/routes-name.enum";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Header,
+  },
+  setup() {
+    const router = useRouter();
+
+    function goToNewsFeed() {
+      router.push({
+        name: RouteName.FEED,
+      });
+    }
+
+    return { goToNewsFeed };
+  },
+});
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 10%;
+  font-size: 150px;
+}
+</style>
